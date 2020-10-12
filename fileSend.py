d2e770282e191cc42aa6a106f75ad613978f964e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 18:24:09 +0300

    add select, send and save file

--- fileSend.py
+++ fileSend.py
@@ -1,22 +1,28 @@
 import socket
 import sys
 import os
+import random
 
 def start_client():
 	while True:	
 		sock = socket.socket()
 		sock.connect(('localhost', 1010))
-		message = input("Input: ")
-		if (message == 'exit'):
-			sock.close()
-			break
-		if (message == 'shutdown'):
-			sock.send(message.encode())
-			sock.close()
-			break
-		sock.send(message.encode())
-		data = sock.recv(1024)
-		print(data)
+		file_path = input('Путь к файлу: ')
+		file = open(file_path, "rb")
+		file_size = sys.getsizeof(file)
+		i = 0
+		sock.send(b'BEGIN')
+		print(file_size)
+		while True:
+			print('send')
+			data = file.read(1024) # считать 1024 байт
+			sock.send(data)
+			if not data:
+				print('Breaking from sending data')
+				sock.send(b'ENDED')
+				print('loool')
+				break
+		file.close()
 
 
 
@@ -25,11 +31,25 @@ def start_server():
 	sock = socket.socket()
 	sock.bind(('', 1010))
 	sock.listen(1)
+	file = open(str(random.randint(1,1000))+".txt", 'wb')
 	while True:
+		i = 0
 		conn, addr = sock.accept()
-		while True:
-			data = conn.recv(1024)
-			message = data.decode()
+		data = conn.recv(32)
+		if data == b'BEGIN':
+			print('begin')
+			while True:
+				data = conn.recv(1024)
+				if (data == b'ENDED'):
+					conn.close()
+					file.close()
+					break
+				file.write(data)
+		if (data == b'ENDED'):
+			print('Breaking from file write')
+			file.close()
+			conn.close()
+'''
 			if (message == 'exit'):
 				conn.close()
 				break
@@ -37,7 +57,9 @@ def start_server():
 				sys.exit()
 			if not data:
 				break
+
 			conn.send(data.upper())
+'''		
 
 
 mode = input("Запустить как:\n1)Клиент\n2)Сервер:\nВаш выбор: ")
